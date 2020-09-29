abb653cf252539e6ad75bd8737ff7d3a3980ed13
gloria <cmxgloria@gmail.com>
2020-09-29 12:02:49 +1000

    updated mock global function

--- util.test.js
+++ util.test.js
@@ -1,6 +1,7 @@
 // first run printTitle is failed because async not promise to resolve the data, so we need to run test of loadTitle first
 
 // jest.mock("./__mocks__/http.js");
+// dont need to use above cause for mocks files, jest use them global then everywhere can access it
 const { loadTitle } = require("./util");
 
 test("should print an uppercase text ", () => {
